930bc860a5d89c7975602561aec28f8342aab388
Mario Simaremare <mariosimaremare@gmail.com>
2020-12-03 17:14:42 +0700

    Add more action

--- bank-client.php
+++ bank-client.php
@@ -16,7 +16,7 @@ $client = new Client([
 // See the detail of an account.
 // The result is different when the request is authenticated
 // and when it is not.
-// See [the documentation](https://github.com/itdel-ppw/simple-bank/docs/account-detail.md) for detail information.
+// See [the documentation](https://github.com/itdel-ppw/simple-bank/blob/master/docs/account-detail.md) for detail information.
 $response = $client->request('GET', '/accounts/jaksem');
 
 if ($response->getStatusCode() !== 200) {
@@ -30,7 +30,7 @@ print_r($account);
 
 // Retrieve all transactions related to the customer
 // by utilizing HATEOAS info from the previous result.
-// See [the documentation](https://github.com/itdel-ppw/simple-bank/docs/transaction-list.md) for detail information.
+// See [the documentation](https://github.com/itdel-ppw/simple-bank/blob/master/docs/transaction-list.md) for detail information.
 $response = $client->request('GET', $account->data->transactions_uri);
 
 if ($response->getStatusCode() !== 200) {
@@ -44,7 +44,7 @@ print_r($transactions);
 
 // Retrieve a transaction related to the customer
 // by utilizing HATEOAS info from the previous result.
-// See [the documentation](https://github.com/itdel-ppw/simple-bank/docs/transaction-detail.md) for detail information.
+// See [the documentation](https://github.com/itdel-ppw/simple-bank/blob/master/docs/transaction-detail.md) for detail information.
 $response = $client->request('GET', $transactions->data[0]->transaction_uri);
 
 if ($response->getStatusCode() !== 200) {
@@ -54,5 +54,24 @@ if ($response->getStatusCode() !== 200) {
 
 $transaction = json_decode($response->getBody()->getContents());
 print_r($transaction);
+
+// Issue a new transaction.
+// See [the documentation](https://github.com/itdel-ppw/simple-bank/blob/master/docs/transaction-issue.md) for detail information.
+$response = $client->request('POST', '/transactions/issue', [
+    'json' => [
+        'recipient'=> 'wirosableng',
+        'amount'=> 4.4,
+        'notes'=> 'Beli baut sepeda'
+    ]
+]);
+
+if ($response->getStatusCode() !== 201) {
+    echo("something went wrong.");
+    die;
+}
+
+// Since the response is JSON encoded, it has to be decoded.
+$new_transaction = json_decode($response->getBody()->getContents());
+print_r($new_transaction);
 
 // Write your own codes...
